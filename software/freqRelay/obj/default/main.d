b4aeb0163c835af2e9d59cace4bd20d4a8a9b6e3
JinkaiZ <jzha541@aucklanduni.ac.nz>
2022-04-22 01:21:16 +1200

    implement the basic switch & red LED functions

--- software/freqRelay/obj/default/main.d
+++ software/freqRelay/obj/default/main.d
@@ -8,10 +8,11 @@ obj/default/main.o: main.c ../freqRelay_bsp//HAL/inc/sys/alt_irq.h \
  ../freqRelay_bsp//HAL/inc/sys/alt_llist.h \
  ../freqRelay_bsp//HAL/inc/priv/alt_dev_llist.h \
  ../freqRelay_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ ../freqRelay_bsp//drivers/inc/altera_avalon_pio_regs.h \
  FreeRTOS/FreeRTOS.h FreeRTOS/FreeRTOSConfig.h FreeRTOS/projdefs.h \
  FreeRTOS/portable.h FreeRTOS/deprecated_definitions.h \
  FreeRTOS/portmacro.h FreeRTOS/mpu_wrappers.h FreeRTOS/task.h \
- FreeRTOS/list.h FreeRTOS/queue.h
+ FreeRTOS/list.h FreeRTOS/queue.h FreeRTOS/semphr.h FreeRTOS/queue.h
 
 ../freqRelay_bsp//HAL/inc/sys/alt_irq.h:
 
@@ -39,6 +40,8 @@ obj/default/main.o: main.c ../freqRelay_bsp//HAL/inc/sys/alt_irq.h \
 
 ../freqRelay_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
 
+../freqRelay_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
 FreeRTOS/FreeRTOS.h:
 
 FreeRTOS/FreeRTOSConfig.h:
@@ -56,5 +59,9 @@ FreeRTOS/mpu_wrappers.h:
 FreeRTOS/task.h:
 
 FreeRTOS/list.h:
+
+FreeRTOS/queue.h:
+
+FreeRTOS/semphr.h:
 
 FreeRTOS/queue.h:
